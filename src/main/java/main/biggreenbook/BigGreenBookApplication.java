2a765f5fd7a487cb031f5c8c736afc7f775fab13
Sodacooky <sodacooky@foxmail.com>
2022-03-08 17:14:47 +0800

    init

---
+++ src/main/java/main/biggreenbook/BigGreenBookApplication.java
@@ -0,0 +1,13 @@
+package main.biggreenbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigGreenBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigGreenBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/main/biggreenbook/BigGreenBookApplicationTests.java
@@ -0,0 +1,13 @@
+package main.biggreenbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigGreenBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
