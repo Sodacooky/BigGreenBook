c16197ff254afbeac2b528aa1d399d1b31b610d2
粒粒醇香 <809990264@qq.com>
2022-04-06 22:24:29 +0800

    新增vo类，前端用户卡片

---
+++ src/main/java/main/biggreenbook/entity/vo/UserCard.java
@@ -0,0 +1,16 @@
+package main.biggreenbook.entity.vo;
+
+public class UserCard {
+    //用户uid
+    private String userUid;
+    //用户昵称
+    private String userNickname;
+    //用户头像
+    private String userAvatarPath;
+    //发布笔记数量
+    private Integer contentAmount;
+    //粉丝数量
+    private Integer fansAmount;
+    //该用户是否已关注
+    private Integer isStatus;
+}
