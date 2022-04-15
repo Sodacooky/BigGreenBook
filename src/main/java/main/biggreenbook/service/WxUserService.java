77747def3271250410bcfa0a7d440b31daab1072
粒粒醇香 <809990264@qq.com>
2022-04-15 20:11:07 +0800

    1.PreviewCard首页与搜索拆分，service层、controller层完善。 2.UserCard亦修改完成

--- src/main/java/main/biggreenbook/service/WxUserService.java
+++ src/main/java/main/biggreenbook/service/WxUserService.java
@@ -27,18 +27,20 @@ public class WxUserService {
     @Autowired
     private StaticMappingHelper staticMappingHelper;
 
-    public List<UserCard> getUserCards(int queryId, Map<String, Object> map) {
+    public List<UserCard> getUserCards(int queryId, Map<String,Object> map) {
         ArrayList<UserCard> result = new ArrayList<>();
 
         //如果是第一页，那么需要解决多余的部分数据
-        if ((int) map.get("pageNum") == 0 && PAGESIZE < queryId) {
-            result.addAll(userMapper.getUserCardByAmount(map));
+        if ((int)map.get("pageNum") == 0 && PAGESIZE < queryId) {
+            result.addAll(userMapper.getUserCardBySearch(map));
         }
         //计算逆序页，获得数据
         int pageAmount = queryId < PAGESIZE ? 1 : queryId / PAGESIZE;
-        int actualPage = pageAmount - 1 - (int) map.get("pageNum");
+        int actualPage = pageAmount - 1 - (int)map.get("pageNum");
 
-        map.replace("pageNum", actualPage);
+        map.replace("amount",PAGESIZE);
+        map.replace("pageNum",actualPage);
+
         result.addAll(userMapper.getUserCardBySearch(map));
 
         //路径映射
