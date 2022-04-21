e97262d83637babab35ba19e47c782c53f230123
HanzyRin <1124869905@qq.com>
2022-04-21 16:52:03 +0800

    加了获得消息和删除消息接口

--- src/main/java/main/biggreenbook/service/MessageService.java
+++ src/main/java/main/biggreenbook/service/MessageService.java
@@ -1,9 +1,11 @@
 package main.biggreenbook.service;
 
 import main.biggreenbook.entity.dao.MessageMapper;
+import main.biggreenbook.entity.pojo.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -11,24 +13,25 @@ public class MessageService {
     @Autowired
     MessageMapper messageMapper;
 
+    // 给某个用户发消息
     public int setMessage(Map<?,?> map) {
         return messageMapper.setMessage(map);
     }
 
+    // 给所有用户发消息
     public int sendAllUser(Map<?,?> map) {
         messageMapper.getAllUid();
         return messageMapper.sendAllUser(map);
     }
 
-//    public int suspendMessage(Map<?, ?> map) {
-//        return messageMapper.suspendMessage(map);
-//    }
-//
-//    public int deleteContentMessage(Map<?, ?> map) {
-//        return messageMapper.deleteContentMessage(map);
-//    }
-//
-//    public int restoreMessage(Map<?, ?> map) {
-//        return restoreMessage(map);
-//    }
+    // 获得某个用户收到的全部消息
+    public List<Message> getMessage(Map<?, ?> map) {
+        return messageMapper.getMessage(map);
+    }
+
+    // 用户删除收到的某条消息
+    public int deleteMessage(Map<?, ?> map) {
+        return messageMapper.deleteMessage(map);
+    }
+
 }
