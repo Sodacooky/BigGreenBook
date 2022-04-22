f94a5d151768efb7febb12adc2c2723365fa6bed
粒粒醇香 <809990264@qq.com>
2022-04-22 10:06:33 +0800

    在sql中将所有$更改为#

--- src/test/java/main/biggreenbook/entity/dao/ContentMapperTest.java
+++ src/test/java/main/biggreenbook/entity/dao/ContentMapperTest.java
@@ -81,46 +81,46 @@ public class ContentMapperTest {
 
     @Test
     public void getContentInfoTest() {
-        ContentInfo contentInfo = contentMapper.getContentInfo("1", "1");
-
+        ContentInfo contentInfo = contentMapper.getContentInfo("obrK14iCCFuFye29bq-dV7BJU9l4", "obrK14iCCFuFye29bq-dV7BJU9l4");
         System.out.println(contentInfo);
     }
 
 
     @Test
     public void updateLikeAmountTest() {
+        contentMapper.updateLikeAmount(-1,"obrK14iCCFuFye29bq-dV7BJU9l4");
     }
 
     @Test
     public void addLikesTest() {
-        contentMapper.addLikes("content", "1111", "1",new Timestamp(new Date().getTime()));
+        contentMapper.addLikes("content", "obrK14iCCFuFye29bq-dV7BJU9l4", "obrK14iCCFuFye29bq-dV7BJU9l4",new Timestamp(new Date().getTime()));
     }
 
     @Test
     public void subLikesTest() {
-        contentMapper.subLikes("1", "1");
+        contentMapper.subLikes("obrK14iCCFuFye29bq-dV7BJU9l4", "obrK14iCCFuFye29bq-dV7BJU9l4");
     }
 
     @Test
     public void queryLikeAmountTest() {
-        contentMapper.queryLikeAmount("1");
+        contentMapper.queryLikeAmount("obrK14iCCFuFye29bq-dV7BJU9l4");
     }
 
     @Test
     public void addReportContentTest() {
         Timestamp date = new Timestamp(new Date().getTime());
-        contentMapper.addReportContent("1", "1", "不可奉告", date);
+        contentMapper.addReportContent("obrK14iCCFuFye29bq-dV7BJU9l4", "obrK14iCCFuFye29bq-dV7BJU9l4", "不可奉告", date);
     }
 
     @Test
     public void addConllectionTest() {
         Timestamp date = new Timestamp(new Date().getTime());
-        contentMapper.addCollection("1", "1", date);
+        contentMapper.addCollection("obrK14iCCFuFye29bq-dV7BJU9l4", "obrK14iCCFuFye29bq-dV7BJU9l4", date);
     }
 
     @Test
     public void deleteCollectionTest() {
-        contentMapper.deleteCollection("1", "1");
+        contentMapper.deleteCollection("obrK14iCCFuFye29bq-dV7BJU9l4", "obrK14iCCFuFye29bq-dV7BJU9l4");
     }
 
 }
