4ec1793e0761284a2f3983686cbd9f4b4a358068
Sodacooky <sodacooky@foxmail.com>
2022-04-24 14:29:01 +0800

    更正了获取用户系统消息问题。

--- src/main/java/main/biggreenbook/controller/MessageController.java
+++ src/main/java/main/biggreenbook/controller/MessageController.java
@@ -3,7 +3,6 @@ package main.biggreenbook.controller;
 import main.biggreenbook.entity.pojo.Message;
 import main.biggreenbook.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -19,20 +18,35 @@ public class MessageController {
 
     /**
      * 用户获得收到的全部消息
+     *
      * @param uid 用户的uid
      * @return 返回该用户收到的全部消息
+     * @deprecated use getMyMessages() instead
      */
+    @Deprecated
     @GetMapping("/get/{uid}")
     public List<Message> getMessage(@PathVariable String uid) {
         Map<String, String> map = new HashMap<>();
         map.put("uid", uid);
 
-       return messageService.getMessage(map);
+        return messageService.getMessage(map);
     }
 
 
     /**
+     * get all the message to the user(myself)
+     *
+     * @param customCode user custom login state string
+     * @return the "message" list
+     */
+    @GetMapping("/get_my_msg")
+    public List<Message> getMyMessages(@RequestParam("customCode") String customCode) {
+        return messageService.getMyMessages(customCode);
+    }
+
+    /**
      * 删除用户选择的通知
+     *
      * @param mid 用户要删除的通知的mid
      */
     @GetMapping("/delete/{mid}")
@@ -40,6 +54,6 @@ public class MessageController {
         Map<String, Integer> map = new HashMap<>();
         map.put("mid", mid);
 
-       return messageService.deleteMessage(map);
+        return messageService.deleteMessage(map);
     }
 }
--- src/main/java/main/biggreenbook/service/MessageService.java
+++ src/main/java/main/biggreenbook/service/MessageService.java
@@ -2,9 +2,12 @@ package main.biggreenbook.service;
 
 import main.biggreenbook.entity.dao.MessageMapper;
 import main.biggreenbook.entity.pojo.Message;
+import main.biggreenbook.utils.RedisHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,24 +17,46 @@ public class MessageService {
     MessageMapper messageMapper;
 
     // 给某个用户发消息
-    public int setMessage(Map<?,?> map) {
+    public int setMessage(Map<?, ?> map) {
         return messageMapper.setMessage(map);
     }
 
     // 给所有用户发消息
-    public int sendAllUser(Map<?,?> map) {
+    public int sendAllUser(Map<?, ?> map) {
         messageMapper.getAllUid();
         return messageMapper.sendAllUser(map);
     }
 
     // 获得某个用户收到的全部消息
+
+    /**
+     * @param map
+     * @return
+     * @deprecated use getMyMessages() instead
+     */
+    @Deprecated
     public List<Message> getMessage(Map<?, ?> map) {
         return messageMapper.getMessage(map);
     }
+
+    public List<Message> getMyMessages(String customCode) {
+        //check customCode
+        if (!redisHelper.hasCustomCode(customCode)) return new ArrayList<>();
+        //fill sql params
+        HashMap<String, String> params = new HashMap<>();
+        params.put("uid", redisHelper.getUidFromCustomCode(customCode));
+        //do query
+        return messageMapper.getMessage(params);
+    }
+
 
     // 用户删除收到的某条消息
     public int deleteMessage(Map<?, ?> map) {
         return messageMapper.deleteMessage(map);
     }
+
+
+    @Autowired
+    private RedisHelper redisHelper;
 
 }
